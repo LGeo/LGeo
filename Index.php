9a59fc1a8ee986c950c8385d3ea5bf3232557d5c
PabloCoronadoMora <mtropablocoronado@gmail.com>
2016-05-26 12:07:07 -0500

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="Librerias/StyleEditForm.css"/>
+<title>LGeo Location</title>
+</head>
+											<span class="mA">Register</span><span class="mB">Help</span>
+                          
+                          <h1 class="hA">LGeo Location</h1>
+                          <h2 class="hB">finding the important</h2>
+<body class="bo">
+	<form enctype="" class="contact_form" action="ProcesaIngreso.php" id="contact_form" method="post">
+     <div> 
+        <ul> 
+  <li>  
+     <input class="me"  type="text" name="user" placeholder="User" required/>
+ </li> 
+  <li> 
+     <input class="me"  type="password" name="pass" placeholder="Password" required/>           
+ </li> 
+ <li> 
+     <button class="submit" type="submit">Log in</button> 
+ </li> 
+         </ul> 
+      </div>     
+     </form>
+</body>
+</html>
+
+<style type="text/css">
+@import url(https://fonts.googleapis.com/css?family=Poiret+One);
+.bo { background-image:url(pics/estacion-espacial.jpg); background-size: cover; background-repeat:no-repeat;}
+.fo {position:absolute;top:50%;left:36.5%;background:#999;opacity:0.6;width:30%;height:40%;
+border-radius: 40px / 40px;}
+.fo li {list-style: none; height:20%;}
+.fo1 { position:absolute; top:25%; left:9%; width:80%;}
+.lo { width:100%; height:50%; }
+.hA {font-family: 'Poiret One', cursive; color:#FFF; font-size:68px; position:absolute; top:20%; left:36%;}
+.hB{font-family:'Poiret One',cursive;color:#FFF;font-size:28px;position:absolute;top:33%;left:42%;transform:scaleX(1.6)}
+.mA{font-family: 'Poiret One', cursive; color:#FFF; font-size:20px; position:absolute; left:70%; top:5%; }
+.mB{font-family: 'Poiret One', cursive; color:#FFF; font-size:20px; position:absolute; left:80%; top:5%; }
+</style>
